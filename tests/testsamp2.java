26a5694d7cb7061b0e591b7abb11dac911fe4090
Kotireddy-anikalla <kotireddyanikalla@gmail.com>
2026-07-23 14:25:30 +0530

    Create SampleVulnerable class with security issues
    Added a sample class demonstrating hardcoded secrets and SQL injection vulnerability.

--- tests/testsamp2.java
+++ tests/testsamp2.java
@@ -1,1 +1,19 @@
 
+import java.sql.*;
+
+public class SampleVulnerable {
+    // Hardcoded Secret
+    private static final String DB_PASSWORD = "SuperSecretPassword123!";
+
+    public void getUser(String userId) {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/db", "root", DB_PASSWORD);
+            Statement stmt = conn.createStatement();
+            // SQL Injection Vulnerability
+            String query = "SELECT * FROM users WHERE id = '" + userId + "'";
+            ResultSet rs = stmt.executeQuery(query);
+        } catch (Exception e) {
+            // Code Smell: Empty catch block
+        }
+    }
+}
